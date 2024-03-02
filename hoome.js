aa688b3e5efbb82d7f8be56fb7e364cb79440842
capgab21 <161921483+capgab21@users.noreply.github.com>
2024-03-02 16:39:23 +0800

    Add files via upload

---
+++ hoome.js
@@ -0,0 +1,143 @@
+const wrapper = document.querySelector(".sliderWrapper");
+const menuItems = document.querySelectorAll(".menuItem");
+
+const products = [
+  {
+    id: 1,
+    title: "Santa Barbara Polo & Racquet Club ACTIVE WEAR",
+    price: 1999,
+    colors: [
+      {
+        
+        img: "WEB image/activewear.png",
+      },
+    ],
+  },
+  {
+    id: 2,
+    title: "Santa Barbara Polo & Racquet Club CASUAL",
+    price: 7999,
+    colors: [
+      {
+        code: "lightgray",
+        img: "WEB image/casual.png",
+      },
+      {
+        code: "green",
+        img: "./Image/jordan2.png",
+      },
+    ],
+  },
+  {
+    id: 3,
+    title: "Santa Barbara Polo & Racquet Club BEACH WEAR",
+    price: 6999,
+    colors: [
+      {
+        code: "lightgray",
+        img: "WEB image/baechwear.png",
+      },
+      {
+        code: "green",
+        img: "./Image/blazer2.png",
+      },
+    ],
+  },
+  {
+    id: 4,
+    title: "Santa Barbara Polo & Racquet Club SPECIAL OCCASION",
+    price: 3999,
+    colors: [
+      {
+        code: "White",
+        img: "WEB image/special occation.png",
+      },
+      {
+        code: "lightgray",
+        img: "./Image/crater2.png",
+      },
+    ],
+  },
+  {
+    id: 5,
+    title: "Santa Barbara Polo & Racquet Club WORK WEAR",
+    price: 999,
+    colors: [
+      {
+        code: "gray",
+        img: ".WEB image/work wear.png",
+      },
+      {
+        code: "black",
+        img: "./Image/hippie2.png",
+      },
+    ],
+  },
+];
+
+let choosenProduct = products[0];
+
+const currentProductImg = document.querySelector(".productImg");
+const currentProductTitle = document.querySelector(".productTitle");
+const currentProductPrice = document.querySelector(".productPrice");
+const currentProductColors = document.querySelectorAll(".color");
+const currentProductSizes = document.querySelectorAll(".size");
+
+menuItems.forEach((item, index) => {
+  item.addEventListener("click", () => {
+    //change the current slide
+    wrapper.style.transform = `translateX(${-100 * index}vw)`;
+
+    //change the choosen product
+    choosenProduct = products[index];
+
+    //change texts of currentProduct
+    currentProductTitle.textContent = choosenProduct.title;
+    currentProductPrice.textContent = "₱" + choosenProduct.price;
+    currentProductImg.src = choosenProduct.colors[0].img;
+
+    //assing new colors
+    currentProductColors.forEach((color, index) => {
+      color.style.backgroundColor = choosenProduct.colors[index].code;
+    });
+  });
+});
+
+currentProductColors.forEach((color, index) => {
+  color.addEventListener("click", () => {
+    currentProductImg.src = choosenProduct.colors[index].img;
+  });
+});
+
+currentProductSizes.forEach((size, index) => {
+  size.addEventListener("click", () => {
+    currentProductSizes.forEach((size) => {
+      size.style.backgroundColor = "white";
+      size.style.color = "black";
+    });
+    size.style.backgroundColor = "black";
+    size.style.color = "white";
+  });
+});
+
+const productButton = document.querySelector(".productButton");
+const payment = document.querySelector(".payment");
+const close = document.querySelector(".close");
+
+productButton.addEventListener("click", () => {
+  payment.style.display = "flex";
+});
+
+close.addEventListener("click", () => {
+  payment.style.display = "none";
+});
+
+// Get the modal
+var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
